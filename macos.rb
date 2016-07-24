c64cb49d0e2f0ba016beda1df28f33587e7e46d5
Lorin Hochstein <lhochstein@netflix.com>
2016-07-23 17:53:25 -0700

    Initial commit

---
+++ macos.rb
@@ -0,0 +1,15 @@
+cheatsheet do
+    title 'macOS'
+    docset_file_name 'macOS'
+    keyword 'mac'
+    category do
+        id 'Text'
+        entry do
+            name 'Special characters'
+            notes <<-'END'
+            Edit -> Emoji & Symbols (^⌘ space)
+            END
+        end
+    end
+end
+
